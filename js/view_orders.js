8cc253bcdb38940f7c5edb05909def29a0cca240
muhwezideo <aggrey256@gmail.com>
2018-10-10 17:18:49 +0200

    [Delivers #161090830] add logic to fecth orders from api backend

--- js/view_orders.js
+++ js/view_orders.js
@@ -1,29 +1,47 @@
 
 var token=localStorage.getItem('access-token')
-var url="https://fast-foods-api-main.herokuapp.com/api/v2/orders"
-fetch(url,{
+
+fetch('https://fast-foods-api-main.herokuapp.com/api/v2/orders',{
     method:'GET',
-    headers:{
-        Authorization:`Bearer ${token}`
-    }
-        })
-     .then(response => response.json())
-  .then(data => {
-    console.log(data)
-  });
+        headers:{
+       Authorization:`Bearer ${token}`
+}
+})
+.then((res) => { return res.json() })
+.then(response=>{
+ var orders=[];
+// console.log(orders)
+ for (const [key, value] of Object.entries(response)) {
+    // console.log(key, value);
+    for (var i = 0; i <value.length; i++) {
+        // console.log(value[i])
+        orders.push(value[i])
+     
+    };
+  };
+function ui(value){
+ for (var i = 0;i <value.length; i++){
+    console.log(value)
+        let content = document.getElementById('result')
 
+        content.innerHTML+= `
+        <div>
+        <table>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        <tr>
+        <th>orderId</th>
+        <th>user_id<th>
+        </tr>
+        
+        <tr>
+        <td> ${value[i].orderid}</td>
+        <td> ${value[i].user_id}</td>
+        <tr>
+        </table>
+        </div> 
+        
+        `  
+    };
+};
+ui(orders);
+});
