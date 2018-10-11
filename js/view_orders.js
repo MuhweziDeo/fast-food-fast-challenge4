7b9b120ba8b41c806983e576065d61d83e37a3ef
muhwezideo <aggrey256@gmail.com>
2018-10-11 21:33:42 +0200

    [Delivers #161090827]fix returned data

--- js/view_orders.js
+++ js/view_orders.js
@@ -1,7 +1,9 @@
 
 var token=localStorage.getItem('access-token')
+url='https://fast-foods-api-main.herokuapp.com/api/v2/orders'
+urls=[]
 
-fetch('https://fast-foods-api-main.herokuapp.com/api/v2/orders',{
+fetch(url,{
     method:'GET',
         headers:{
        Authorization:`Bearer ${token}`
@@ -13,23 +15,28 @@ fetch('https://fast-foods-api-main.herokuapp.com/api/v2/orders',{
  for (const [key, value] of Object.entries(response)) {
     for (var i = 0; i <value.length; i++) {
         orders.push(value[i])
+        var id=value[i].orderid
+        urls.push(`https://fast-foods-api-main.herokuapp.com/api/v2/orders/${id}`);
+    for(i =0; i<id.length; i++){
+        console.log(i)
+    }
+    
     };
   };
 
+
+
+// outputs api data to frontend
 function ui(value){
  for (var i = 0;i <value.length; i++){
         let content = document.getElementById('result')
-
+        // console.log(urls)
         content.innerHTML+= `
         <div>
         <h2>Order ${value[i].orderid} </h1>
-        <p>orderID: ${value[i].orderid}</p>
-        <p>userID: ${value[i].user_id}</p>
-        <p>Meal: ${value[i].meal_name}</p>
+        <p>
+        <a href="${url}" id='${value[i].orderid}'> orderID: ${value[i].orderid}<a/></p>
         <p>order-status: ${value[i].status}</p>
-        <p>quantity:${value[i].quantity}</p>
-        <p>location:${value[i].location}</p>
-        <p> Date:${value[i].order_date}</p>
         <form id="accept">
         <input class="btn btn-primary" type='submit' value="accept" id='${value[i].orderid}'>
         </form>
@@ -37,13 +44,12 @@ function ui(value){
         <form id="reject">
         <input class="btn btn-danger" type='submit' value="reject" id='${value[i].orderid}'>
         </form>
-
-        </div> 
-        
-        ` 
-    };
+        </div>    
+        `     
+    };  
 };
 ui(orders);
+
 });
 
 
