3fe8d6ff6da2a145723114161a94139095bcc900
muhwezideo <aggrey256@gmail.com>
2018-10-12 21:12:59 +0200

    [Delivers#161090826] add javascript logic to handle adding a meal option

---
+++ js/menu.js
@@ -0,0 +1,85 @@
+var token = localStorage.getItem('access-token')
+url = 'https://fast-foods-api-main.herokuapp.com/api/v2/menu'
+let urls = []
+// add meal
+document.getElementById('menu-form').addEventListener('submit', add_meal)
+
+function add_meal(event) {
+    event.preventDefault()
+    var mealNameData = document.getElementById('meal_name').value;
+    var priceData = document.getElementById('price').value;
+    var mealData = {
+        meal_name: mealNameData,
+        price: priceData,
+    };
+
+    fetch('https://fast-foods-api-main.herokuapp.com/api/v2/menu', {
+            method: 'post',
+            headers: {
+                'content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify(mealData)
+
+        }).then(res => res.json())
+        .then(response => {
+            alert(response['message']);
+            window.location.href = "admin_panel_items.html"
+        });
+}
+
+// get menu
+fetch(url, {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+    .then((res) => {
+        return res.json()
+    })
+    .then(response => {
+        console.log(response)
+        for (const [key, value] of Object.entries(response)) {
+            for (var i = 0; i < value.length; i++) {
+                console.log(value[i])
+                let content = document.getElementById('menu')
+
+                content.innerHTML += `
+        <table id="food-items-table">
+                        <tr>
+                            <th>ID</th>
+                            <th> Name</th>
+                            <th> Price</th>
+                            <th>Status</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+
+                        </tr>
+                        <tr>
+                            <td>${value[i].meal_id}</td>
+                            <td>${value[i].meal_name}</td>
+                            <td>${value[i].price}</td>
+                            <td>${value[i].meal_status}</td>
+                            <td><button id='${value[i].meal_id}' class="btn-primary">Edit</button></td>
+                            <td><button  id='${value[i].meal_id}' class="btn-danger">Delete</button></td>
+                        </tr>
+                        <tr>
+         </table>
+         
+        `;
+
+
+            };
+            var results = document.getElementById('menu')
+            results.onclick = e => {
+
+                // window.location.href = "login.html"
+                var id = e.target.attributes.getNamedItem("id").value;
+                // console.log(c);
+                localStorage.setItem('meal_id', id)
+                window.location.href = "menu.html"
+            }
+        };
+
+    });
