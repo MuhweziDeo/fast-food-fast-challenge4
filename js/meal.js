6ed375066cadf1706e653cec96b5727484c0ed16
muhwezideo <aggrey256@gmail.com>
2018-10-12 21:42:56 +0200

    [Finishes #161090826] add logic to handle deleting a meal option

--- js/meal.js
+++ js/meal.js
@@ -1,5 +1,6 @@
 var id=localStorage.getItem('meal_id')
 var token=localStorage.getItem("access-token")
+// update meal
 document.getElementById('update-meal-form').addEventListener('submit', update_meal)
 function update_meal(event) {
     event.preventDefault()
@@ -16,6 +17,25 @@ function update_meal(event) {
                 Authorization: `Bearer ${token}`
             },
             body: JSON.stringify(mealData)
+
+        }).then(res => res.json())
+        .then(response => {
+            alert(response['message']);
+            window.location.href = "admin_panel_items.html"
+        });
+}
+
+// delete meal
+document.getElementById('delete').addEventListener('click', delete_meal)
+function delete_meal(event) {
+    event.preventDefault()
+    console.log('hhhh')
+    fetch(`https://fast-foods-api-main.herokuapp.com/api/v2/meal/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            }
 
         }).then(res => res.json())
         .then(response => {
