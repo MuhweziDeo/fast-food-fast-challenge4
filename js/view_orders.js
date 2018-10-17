5ec7a7034b390e75efb8282e4553d33b394fb9d1
muhwezideo <aggrey256@gmail.com>
2018-10-17 16:05:32 +0200

    [Delivers #161197480]add response if there are no users placed

--- js/view_orders.js
+++ js/view_orders.js
@@ -12,44 +12,57 @@ fetch(url,{
 .then((res) => { return res.json() })
 .then(response=>{
     var user=localStorage.getItem('logged_in_user')
-    console.log(user)
     let loggedInUser=document.getElementById('current_user')
     loggedInUser.innerHTML=`
     <p>Current User: ${user}</p>
     `;
- for (const [key, value] of Object.entries(response)) {
-    for (var i = 0; i <value.length; i++) {
-        // console.log(value[i])
-        orders=value[i]
-       
-        var id=value[i].orderid
+    console.log(response['message'])
+if(response['message']=='No orders have been placed yet'){
 
-        let content = document.getElementById('result')
+    let content = document.getElementById('no-orders-response')
 
-        content.innerHTML+= `
-        <div class="all-orders" id="${id}">
-        <p> orderID: ${value[i].orderid}</p>
-        <p>order-status: ${value[i].status}</p>
-        <button class="btn-primary" id="${id}" >View More Details</button>
-        </div>    
-        `;
-     
-      
-      
-    };
+    content.innerHTML+= `
+    <h2>No orders Have Been Placed Yet<h2>  
+    `;
+ 
 
-
- var results= document.getElementById('result')
-    results.onclick=e=>{       
-        
-        // window.location.href = "login.html"
-       var id= e.target.attributes.getNamedItem("id").value;
-        // console.log(c);
-        localStorage.setItem('orderid',id)
-        window.location.href = "orders.html"
-    }
-  };
-
+}
+else{
+    for (const [key, value] of Object.entries(response)) {
+        for (var i = 0; i <value.length; i++) {
+            // console.log(value[i])
+            orders=value[i]
+           
+            var id=value[i].orderid
+    
+            let content = document.getElementById('result')
+    
+            content.innerHTML+= `
+            <div class="all-orders" id="${id}">
+            <p> orderID: ${value[i].orderid}</p>
+            <p>order-status: ${value[i].status}</p>
+            <button class="btn-primary" id="${id}" >View More Details</button>
+            </div>    
+            `;
+         
+          
+          
+        };
+    
+    
+     var results= document.getElementById('result')
+        results.onclick=e=>{       
+            
+            // window.location.href = "login.html"
+           var id= e.target.attributes.getNamedItem("id").value;
+            // console.log(c);
+            localStorage.setItem('orderid',id)
+            window.location.href = "orders.html"
+        }
+      };
+    
+}
+ 
 });
     
 
