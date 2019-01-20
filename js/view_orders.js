aca2388734af20a7307f6925897f68746daf486f
muhwezi deo <aggrey256@gmail.com>
2019-01-20 10:48:21 +0300

    refactor view orders feature for admin

--- js/view_orders.js
+++ js/view_orders.js
@@ -1,40 +1,47 @@
 let token=localStorage.getItem('token')
+let username=localStorage.getItem('username')
 let orders=document.getElementById('result');
+
 get('https://fast-foods-api-main.herokuapp.com/api/v2/orders',token).then(
     data=>{
-        console.log(data)
         for(const[key,value] of Object.entries(data)){
             let ordersList=value;
+           
+            let orderNumber =ordersList.length
+            let total=document.getElementById('order-total');
+
+            total.innerHTML=`<p>Total orders: ${orderNumber} </p>`
+
             ordersList.forEach(element => {
                 let orders=document.getElementById('result');
-                orders.innerHTML+=`
-                    <div class="card">
+                orders.innerHTML+=
+                `
+                    <div class="card" id='order-info'>
                     <img src="http://lorempixel.com/400/200/food" alt="Avatar" style="width:100%">
                     <div class="container">
                         <h4><b>status: ${element['status']}</b></h4> 
                         <p>orderID: ${element['orderid']}</p> 
                         <p>ordered By: ${element['user_id']}
                         <br>
-                        <button  class="btn btn-primary handleOrderButton" id=${element['order_id']}>Handle</button>
+                        <button type='submit'  class="btn btn-primary handleOrderButton" id=${element['orderid']}>Handle</button>
                     </div>
                     </div>
                 `
-            });
+            }
+        )};
+        var orderDetail= document.getElementById('result')
+        orderDetail.onclick=e=>{  
+            
+            var orderID=e.target.attributes.getNamedItem('id').value
+            localStorage.setItem('orderid',orderID)
+            window.location.href = "orders.html"
+                    
         }
     }
+   
 )
 
-
-//      var results= document.getElementById('result')
-//         results.onclick=e=>{       
-            
-//             // window.location.href = "login.html"
-//            var id= e.target.attributes.getNamedItem("id").value;
-//             // console.log(c);
-//             localStorage.setItem('orderid',id)
-//             window.location.href = "orders.html"
-//         }
-//       }}
+    
 
     
 
